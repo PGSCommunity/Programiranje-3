bbeb4ca81e30858e3d2bf8eb638ce331944a467a
Danin Sadžak <daninsadzak@gmail.com>
2023-09-15 18:05:02 +0200

    Vjezbe - Klase - Postavljen 1. zadatak

--- Vjezbe/Klase/1.cpp
+++ Vjezbe/Klase/1.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    
+
     return 0;
 }
+/*
+Kreirati klasu auto sa varijablama javnog tipa brzina, model, boja. U main funkciji kreirati dva objekta koristeci
+objekte dodijeliti vrijednost variablama po zelji zatim ispisati dvije recenice:
+Ja vozim ... auto, ... boje i jurim ... brzinom na sat.
+Moj drug vozi ... auto, ... boje i ne juri bas jer vozi ... na sat.
+*/
