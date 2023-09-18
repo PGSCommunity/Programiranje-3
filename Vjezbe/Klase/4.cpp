b88e91636766b0a781dd3d5f0c2ab6be9ed5c0b3
Danin Sadžak <daninsadzak@gmail.com>
2023-09-19 00:08:07 +0200

    Vjezbe - Klase - Zavrsen 4. zadatak

--- Vjezbe/Klase/4.cpp
+++ Vjezbe/Klase/4.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Kvadrat {
+    public:
+        double povrsina(double stranica);
+        double obim(double stranica);
+        double dijagonala(double stranica);
+};
 
+double Kvadrat::povrsina(double stranica) {
+    return pow(stranica, 2);
+}
+
+double Kvadrat::obim(double stranica) {
+    return 4 * stranica;
+}
+
+double Kvadrat::dijagonala(double stranica) {
+    return stranica * sqrt(2);
+}
+
+int main() {
+    Kvadrat k;
+
+    double stranica;
+    cout << "Unesite stranicu kvadrata!\n";
+    cin >> stranica;
+
+    cout << "Povrsina kvadrata iznosi: " << k.povrsina(stranica) << '\n';
+    cout << "Obim kvadrata iznosi: " << k.obim(stranica) << '\n';
+    cout << "Dijagonala kvadrata iznosi: " << k.dijagonala(stranica) << '\n';
     return 0;
 }
 /*
