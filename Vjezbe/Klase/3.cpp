16a70d07240384dbf1c4aa970021ce0a433e9814
Danin Sadžak <daninsadzak@gmail.com>
2023-09-18 23:51:40 +0200

    Vjezbe - Klase - Postavljen 3. zadatak

---
+++ Vjezbe/Klase/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarirati klasu ucenik sa varijablama ime i prosjek. Deklarirati dva objekta nad tom klasom. Prvom dodijeliti
+vrijednosti, a za drugi korisniku omoguciti unos, a u main funkciji ispisati podatke o ucenicima.
+
+Definirati funkciju (metodu) koja ce vratiti koliko godina ucenik ima.
+*/
