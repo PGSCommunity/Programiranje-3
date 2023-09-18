efcc3a4c8960daca67a23b4440d72e1e47def6b4
Danin Sadžak <daninsadzak@gmail.com>
2023-09-19 00:01:32 +0200

    Vjezbe - Klase - Postavljen 4. zadatak

---
+++ Vjezbe/Klase/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarisati klasu Kvadrat sa funkcijama (metodama) povrisna, obim, i dijagonala. U main funkciji ispisati izracun
+vrijednosti.
+*/
