006ecd53636afcbd57240082bb03e232c9418add
Danin Sadžak <daninsadzak@gmail.com>
2023-09-18 22:31:53 +0200

    Vjezbe - Klase - Postavljen 2. zadatak

---
+++ Vjezbe/Klase/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarirati klasu auto sa varijablama model, boja, i brzina. Deklarirati dva objekta nad tom klasom prvom.
+Prvom dodijeliti vrijednosti, za drugi omoguciti korisniku unos, a u main funkciji treba da stoji ispis:
+"Ja vozim ... auto, ... boje i jurim ... kilometara na sat"
+"Moj drug vozi ... auto, ... boje i ne juri bas jer vozi ... kilometara na sat"
+
+Definirati funkciju (metodu) koja ce vratiti iznos novcane kazne za prvog vozaca
+*/
