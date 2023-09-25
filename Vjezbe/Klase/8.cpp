dfc29d6d1fc75419ccb8960d29992989ee4f4613
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:34:01 +0200

    Vjezbe - Klase - Postavljen 8. zadatak

---
+++ Vjezbe/Klase/8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarisati klasu krug sa metodama unos poluprecnika, povrsina i obim. Sve varijable su privatne. U glavnoj
+funkcijni treba se pozvati sve metode.
+*/
