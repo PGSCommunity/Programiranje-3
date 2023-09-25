9289b309fdbe262b1333ae7e2c51d649841a221d
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:10:55 +0200

    Vjezbe - Klase - Postavljen 5. zadatak

---
+++ Vjezbe/Klase/5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarirati klasu trougao i metodu povrsina. Na osnovu unesene osnovice i kraka ispisati povrsinu jednakokrakog 
+trougla. U glavnoj funkciji se unose vrijednosti osnovice i kraka i poziva funkcija pomocu objekta 
+(sama vrijednost povrsine se ispisuje unutar funckije).
+*/
