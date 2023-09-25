fecbab3de55c6231ad5406958307e7e28bcd665e
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:22:23 +0200

    Vjezbe - Klase - Postavljen 6. zadatak

---
+++ Vjezbe/Klase/6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati klasu kalkulator, sa 4 metode, sabiranje, oduzimanje, mnozenje i dijeljenje. U glavnoj funkicji se unose
+dva broja i znak za izbor, i na osnovu izbora znaka prosljedjuju se metodama, i ispisuje rezultat.
+*/
