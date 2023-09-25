965bb1317458b3c121189077dbb04d144c7caf70
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:42:23 +0200

    Vjezbe - Klase - Zavrsen 9. zadatak

--- Vjezbe/Klase/9.cpp
+++ Vjezbe/Klase/9.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Interval {
+    public:
+        int suma(int x, int y);
+        int brojParnih(int x, int y);
+};
 
+int Interval::suma(int x, int y) {
+    int s(0);
+    for(int i = x; i <= y; i++) {
+        s += i;
+    }
+    return s;
+}
+
+int Interval::brojParnih(int x, int y) {
+    int br(0);
+    for(int i = x; i <= y; i++) {
+        if(i % 2 == 0) br++;
+    }
+    return br;
+}
+
+int main() {
+    Interval i;
+    int x, y;
+
+    cout << "Od broja?\n";
+    cin >> x;
+    
+    cout << "Do broja?\n";
+    cin >> y;
+    
+    cout << "Suma brojeva u intervalu: " << i.suma(x, y) << '\n';
+    cout << "Broj parnih brojeva u intervalu: " << i.brojParnih(x, y) << '\n'; 
     return 0;
 }
 /*
