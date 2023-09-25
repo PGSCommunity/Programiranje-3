6fb1090b6e547639839995c901daa147e77dfc27
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:28:57 +0200

    Vjezbe - Klase - Postavljen 7. zadatak

---
+++ Vjezbe/Klase/7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Unutar klase pravougaonik deklarisati dvije metode, povrsina i obim koje racunaju vrijednost povrsine i obima i
+spremaju u variablu c koja je definirana kao privatna.
+*/
