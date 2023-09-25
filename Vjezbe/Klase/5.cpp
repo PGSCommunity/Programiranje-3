7747070986197b48bfd642f87549d6774992cf26
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:20:33 +0200

    Vjezbe - Klase - Zavrsen 5. zadatak

--- Vjezbe/Klase/5.cpp
+++ Vjezbe/Klase/5.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Trougao {
+    public:
+        void povrsina(float a, float b);
+};
 
+void Trougao::povrsina(float a, float b) {
+    int h = sqrt(pow(a, 2) - pow(b/2, 2));
+    cout << "Povrsina iznosi: " << (a * h) / 2 << '\n';
+}
+
+int main() {
+    Trougao t;
+    int a, b;
+    cout << "Unesite osnovicu i krak!\n";
+    cin >> a >> b;
+
+    t.povrsina(a, b);
     return 0;
 }
 /*
