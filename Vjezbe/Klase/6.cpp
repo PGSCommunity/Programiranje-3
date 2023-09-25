6e43e0c91a8a0f2df80fe1ae66ef85577eb3accd
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:27:36 +0200

    Vjezbe - Klase - Zavrsen 6. zadatak

--- Vjezbe/Klase/6.cpp
+++ Vjezbe/Klase/6.cpp
@@ -1,8 +1,58 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Kalkulator {
+    public:
+        int sabiranje(int a, int b);
+        int oduzimanje(int a, int b);
+        int mnozenje(int a, int b);
+        int dijeljenje(int a, int b);
+};
 
+int Kalkulator::sabiranje(int a, int b) {
+    return a + b;
+}
+
+int Kalkulator::oduzimanje(int a, int b) {
+    return a - b;
+}
+
+int Kalkulator::mnozenje(int a, int b) {
+    return a * b;
+}
+
+int Kalkulator::dijeljenje(int a, int b) {
+    return a / b;
+}
+
+int main() {
+    Kalkulator k;
+    int a, b;
+    char c;
+
+    cout << "Unesite brojeve a i b!\n";
+    cin >> a >> b;
+    
+    cout << "Unesite znak za izbor! (+, -, *, /)\n";
+    unos:cin >> c;
+    
+    switch(c) {
+        case '+':
+            cout << k.sabiranje(a, b);
+            break;
+        case '-':
+            cout << k.oduzimanje(a, b);
+            break;
+        case '*':
+            cout << k.mnozenje(a, b);
+            break;
+        case '/':
+            cout << k.dijeljenje(a, b);
+            break;
+        default:
+            cout << "Netacno! Mora te unijeti znak (+, -, *, /)!\nPokusaj te ponovo: ";
+            goto unos;
+    }
     return 0;
 }
 /*
