abae3d831e51fb42bb8fecd5644ba91bc5ff2b6a
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:37:10 +0200

    Vjezbe - Klase - Zavrsen 8. zadatak i napravljen settings.json!

--- Vjezbe/Klase/8.cpp
+++ Vjezbe/Klase/8.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Krug {
+    public:
+        void unosPoluprecnika();
+        float povrsina();
+        float obim();
+    private:
+        const float PI = 3.14;
+        float r;
+};
 
+void Krug::unosPoluprecnika() {
+    cout << "Unesite poluprecnik!\n";
+    cin >> r;
+}
+
+float Krug::povrsina() {
+    return pow(r, 2) * PI;
+}
+
+float Krug::obim() {
+    return 2 * r * PI;
+}
+
+int main() {
+    Krug k;
+    k.unosPoluprecnika();
+    cout << "Povrsina: " << k.povrsina() << '\n';
+    cout << "Obim: " << k.obim() << '\n';
     return 0;
 }
 /*
