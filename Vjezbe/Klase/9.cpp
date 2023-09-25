c02d0947d96e8c3549b128c8694c4bd3a495659c
Danin Sadžak <daninsadzak@gmail.com>
2023-09-25 17:39:01 +0200

    Vjezbe - Klase - Postavljen 9. zadatak

---
+++ Vjezbe/Klase/9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarirati klasu interval koja u javnom dijelu ima metode suma i broj parnih. Metode su sa argumentima a racunaju
+i ispisuju broj parnih brojeva i sumu u intervalu x, y. U glavnoj funkciji se deklarira objekt nad klasom i unose 
+vrijednosti intervala x, y.
+*/
