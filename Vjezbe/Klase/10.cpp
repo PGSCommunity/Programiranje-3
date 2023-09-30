d27acc31f54867704212cd4b0866a69aacee4251
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 18:56:54 +0200

    Vjezbe - Klase - Zavrsen 10. zadatak

--- Vjezbe/Klase/10.cpp
+++ Vjezbe/Klase/10.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Razred {
+    public:
+        int ucenici(int brojUcenika);
+};
+
+int Razred::ucenici(int brojUcenika) {
+    return brojUcenika;
+}
+
 int main() {
+    Razred r;
+    cout << "Broj ucenika u odljeljenju je: " << r.ucenici(23); // Mak ;(
     return 0;
 }
 /*
