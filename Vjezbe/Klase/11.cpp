26bb8a3f088a8d2a575e831a91e4d03aaeafc9db
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 18:58:42 +0200

    Vjezbe - Klase - Postavljen 11. zadatak

---
+++ Vjezbe/Klase/11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Deklarisati klasu TestBroj koja sadrzi metodu ciji je zadatak ispitati da li je broj unesen od strane korisnika 
+pozitivan ili negativan.Zadatak metode je da vrati logicku vrijednost (true or false) a sama poruka se ispisuje
+unutar main funkcije
+*/
