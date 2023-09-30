30cf2cd9c0da21ed94c4cc3bcd85d5e45ce09ca6
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 19:09:54 +0200

    Vjezbe - Klase - Postavljen 13. zadatak

--- Vjezbe/Klase/13.cpp
+++ Vjezbe/Klase/13.cpp
@@ -6,7 +6,7 @@ int main() {
     return 0;
 }
 /*
-Deklarisati klasuProst koja sadrzi metodu ciji je zadatak ispitati da li je broj unesen od strane korisnika prost
+Deklarisati klasu Prost koja sadrzi metodu ciji je zadatak ispitati da li je broj unesen od strane korisnika prost
 ili ne. Zadatak metode je da vrati logicku vrijednost (true or false) a sama poruka se ispisuje unutar main 
 funkcije
 */
