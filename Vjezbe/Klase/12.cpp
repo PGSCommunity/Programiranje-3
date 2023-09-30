a9d0572a63913e867260756f5ef5959c611ef2d9
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 19:05:56 +0200

    Vjezbe - Klase - Postavljen 12. zadatak

---
+++ Vjezbe/Klase/12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarisati klasu "AbsolutnaVrijednost" sa jednom metodom, koja vraca absolutnu vrijednost korisnickog unesenog
+broja bez koristenja cmath biblioteke.
+*/
