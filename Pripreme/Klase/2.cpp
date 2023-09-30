1655773db75143519253a58fe8d20c50fa54cb3e
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 13:21:23 +0200

    Pripreme - Klase - Postavljen 2. zadatak

---
+++ Pripreme/Klase/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci OOP , napisati program koji za učitani poluprečnik, računa  i ispisuje površinu i zapreminu lopte. 
+Pogram sadrzi jednu klasu i dvije metode koje vraćaju vrijednosti. Metode je potrebno definirati unutar klase.
+*/
