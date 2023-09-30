c66589d289631341ea552dc5cd177405acbe2931
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 19:25:22 +0200

    Vjezbe - Klase - Postavljen 15. zadatak

---
+++ Vjezbe/Klase/15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarirati klasu Aritmeticka koja u javnom dijelu ima metode Obrada() i Ispis(), obje metode su bez argumenata. 
+Prva omogucava unos brojeva razlicitih od 0 (unosom 0 radnja se prekida) i sabiranje pozitivnih brojeva,
+a druga ispisuje aritmeticku sredinu unesenih pozitivnih projeva. 
+U glavnoj funkciji se deklarira objekt a i pozivaju navedene metode.
+*/
