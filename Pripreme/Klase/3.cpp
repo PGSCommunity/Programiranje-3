e1e5ee5e1f83615f32b423fccdd5f5dc0d7f399e
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 13:27:37 +0200

    Pripreme - Klase - Postavljen 3. zadatak

---
+++ Pripreme/Klase/3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci OOP , napisati program koji Izračuna
+    a.	broj parnih prirodnih brojeva u intervalu  x do y koji su djeljivi sa z. 
+    b.	proizvod neparnih brojeva u intervalu x do y čiji je kvadrat jednak broju z.
+Program sadrzi jednu klasu i tri metode(prva omogucava unos vrijednosti x, y i z, a druge dvije omogucavaju gore
+navedeni proračun i ispis). Unutar main funkcije se definira objekt nad klasom koji samo poziva metode. Metode 
+je potrebno definirati izvan klase.
+*/
