26e6f979b5efa3e7883a8ccdeff19387cc2bedf9
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 18:54:55 +0200

    Vjezbe - Klase - Postavljen 10. zadatak

---
+++ Vjezbe/Klase/10.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Definirati klasu razred i metodu (izvan definicije klase) koja vraća vrijednost - broj učenika odjeljenja
+*/
