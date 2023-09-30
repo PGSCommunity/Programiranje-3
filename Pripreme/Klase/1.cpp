f2067be79e60c6a33093997046d4fb748331432f
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 13:10:13 +0200

    Pripreme - Klase - Postavljen 1. zadatak

---
+++ Pripreme/Klase/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci OOP , napisati program pomoću kojeg se deklariše klasa sa tri atributa. Naziv Klase je “Skola“ a metoda: 
+naziv_skole, mjesto i broj_ucenika. Atributima prve škole se dodjeljuju vrijednosti, a a vrijednosti atributa 
+druge škole određuje (unosi) korisnik programa.
+Program treba ispisati vrijednosti atributa u oba slučaja.
+*/
