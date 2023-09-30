738a9d06c3d378cc8d54228e4934dd7fd6f2c1c9
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 13:26:22 +0200

    Pripreme - Klase -Zavrsen 2. zadatak

--- Pripreme/Klase/2.cpp
+++ Pripreme/Klase/2.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Lopta {
+    public: 
+        const float PI = 3.14;
+        float povrsina(float poluprecnik) {
+            return 4 * pow(poluprecnik, 2) * PI;
+        }
+        float zapremina(float poluprecnik) {
+            return (4 / 3) * pow(poluprecnik, 3) * PI;
+        }
+};
 
+int main() {
+    Lopta l;
+    float poluprecnik;
+
+    cout << "Unesite poluprecnik lopte!\n";
+    cin >> poluprecnik;
+
+    cout << "Ispis podataka lopte za ucitani poluprecnik:\n";
+    cout << "Povrsina: " << l.povrsina(poluprecnik) << '\n';
+    cout << "Zapremina: " << l.zapremina(poluprecnik) << '\n';
     return 0;
 }
 /*
