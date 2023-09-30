df91affc67954e08d1e2a1522b5cb6fc3fe0e359
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 19:21:20 +0200

    Vjezbe - Klase - Postavljen 14. zadatak

---
+++ Vjezbe/Klase/14.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarirati klasu Tacka koja u javnom dijelu ima metodu Ishodiste() - metoda s argumentima,
+u glavnoj funkciji se deklarira objekt T1, unose koordinate tacke te ispisuje udaljenost tacke od iskodista.
+*/
