7cb8699ef422f4e236c44d2f5958d5e77756f75e
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 13:39:19 +0200

    Pripreme - Klase - Zavrsen 3. zadatak

--- Pripreme/Klase/3.cpp
+++ Pripreme/Klase/3.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Interval {
+    public:
+        int x, y, z;
+        void unosPodataka();
+        void brojParnih();
+        void proizvodNeparnih();
+};
 
+void Interval::unosPodataka() {
+    cout << "Od broja?\n";
+    cin >> x;
+
+    cout << "Do broja?\n";
+    cin >> y;
+
+    cout << "Unesite broj z!\n";
+    cin >> z;
+}
+
+void Interval::brojParnih() {
+    int br(0);
+
+    for(int i = x; i <= y; i++) {
+        if (i % 2 == 0 && i % z == 0) br++;
+    }
+
+    cout << "Broj parnih brojeva djeljivih sa z: " << br << '\n';
+}
+
+void Interval::proizvodNeparnih() {
+    int p(1);
+
+    for(int i = x; i <= y; i++) {
+        if (i % 2 != 0 && pow(i, 2) == z) p *= i;
+    }
+
+    cout << "Proizvod neparnih brojeva ciji je kvadrat jednak broju z: " << p << '\n';
+}
+
+int main() {
+    Interval i;
+    i.unosPodataka();
+    i.brojParnih();
+    i.proizvodNeparnih();
     return 0;
 }
 /*
