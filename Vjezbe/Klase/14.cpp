884206ed8a607bb20a6c93d741afa27cd1fb0297
Danin Sadžak <daninsadzak@gmail.com>
2023-09-30 19:23:53 +0200

    Vjezbe - Klase - Zavrsen 14. zadatak

--- Vjezbe/Klase/14.cpp
+++ Vjezbe/Klase/14.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Tacka {
+    public:
+        float Ishodiste(int a, int b);
+};
 
+float Tacka::Ishodiste(int a, int b) {
+    return sqrt(pow(a, 2) + pow(b, 2));
+}
+
+int main() {
+    Tacka T1;
+    int x, y;
+
+    cout << "Unesite tacke x i y!\n";
+    cin >> x >> y;
+
+    cout << "Udaljenost tacke od ishodista: " << T1.Ishodiste(x, y) << '\n';
     return 0;
 }
 /*
