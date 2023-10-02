39706bb2fa1a706b38cf1a630a02263abf8d42cd
Danin Sadžak <daninsadzak@gmail.com>
2023-10-02 19:58:46 +0200

    Vjezbe - Nasljedjivanje - Postavljen 1. zadatak

---
+++ Vjezbe/Nasljedjivanje/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati klasu Kvadrat koja ima metode "Upis" i "Rezultat" i protected varijablu "a". Kreirati klasu Kub koja 
+nasljedjuje klasu Kvadrat te ima metodu "Rezultat". U glavnom programi napraviti objekt klase Kvadrat, pozvati
+metode, pa napraviti objekt klase Kub, pa pozvati i njene metode.
+*/
