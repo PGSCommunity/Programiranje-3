8cf18497a05d2d35da113cb2211135a09633db87
Danin Sadžak <daninsadzak@gmail.com>
2023-10-02 19:46:48 +0200

    Testovi - Klase - Zavrsen 2. zadatak

--- Testovi/Klase/2.cpp
+++ Testovi/Klase/2.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Kvadar {
+    public:
+        int povrsina(int a, int b, int c);
+        int zapremina(int a, int b, int c);
+        float dijagonala(int a, int b, int c);
+};
 
+int Kvadar::povrsina(int a, int b, int c) {
+    return 2 * (a * b + a * c + b * c);
+}
+
+int Kvadar::zapremina(int a, int b, int c) {
+    return a * b * c;
+}
+
+float Kvadar::dijagonala(int a, int b, int c) {
+    return sqrt(pow(a,2) + pow(b, 2) + pow(c, 2));
+}
+
+int main() {
+    Kvadar k1;
+    int a, b, c;
+    
+    cout << "Unesite stranice a, b, i c!\n";
+    cin >> a >> b >> c;
+
+    cout << "Povrsina kvadra iznosi: " << k1.povrsina(a, b, c) << '\n';
+    cout << "Zapremina kvadra iznosi: " << k1.zapremina(a, b, c) << '\n';
+    cout << "Dijagonala kvadra iznosi: " << k1.dijagonala(a, b, c) << '\n';
     return 0;
 }
 /*
