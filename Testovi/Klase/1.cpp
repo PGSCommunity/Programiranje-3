13560c4b1dcd69934ae6699d3ffe574163650b68
Danin Sadžak <daninsadzak@gmail.com>
2023-10-02 19:34:46 +0200

    Testovi - Klase - Postavljen 1. zadatak

---
+++ Testovi/Klase/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati klasu Auto koja sadrzi tri atributa: marka, model i godiste, te tri metode: ispisi_marku(), ispisi_model(),
+i ispisi_godiste() (zadatak metoda je omoguciti ispis vrijednosti koja je proslijedena iz glavne funkcije. Npr.
+Prva funkcija bi trebala omoguciti ispis u formi "Marka unesenog automobila je _____")
+U glavnoj funkciji formiraju se dva objekta nad klasom (auto1 i auto2). Pomocu prvog objekta kreator ovog programa
+dodjeljuje proizvoljne vrijednosti atributima i omogucava ispis istih. Drugi objekt omogucava korisnicki unos
+vrijednosti atributa, cije se vrijednosti prosljeduju u funkcije.
+*/
