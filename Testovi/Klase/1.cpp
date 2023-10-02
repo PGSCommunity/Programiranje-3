7f06bb3df7174f5401d9dc698131e4ab95f91d84
Danin Sadžak <daninsadzak@gmail.com>
2023-10-02 19:41:17 +0200

    Testovi - Klase - Zavrsen 1. zadatak

--- Testovi/Klase/1.cpp
+++ Testovi/Klase/1.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+class Auto {
+    public:
+        string marka;
+        string model;
+        int godiste;
+        void ispisi_marku(string marka);
+        void ispisi_model(string model);
+        void ispisi_godiste(int godiste);
+};
 
+void Auto::ispisi_marku(string marka) {
+    cout << "Marka unesenog auta je: " << marka << '\n';
+}
+
+void Auto::ispisi_model(string model) {
+    cout << "Model unesenog auta je: " << model << '\n';
+}
+
+void Auto::ispisi_godiste(int godiste) {
+    cout << "Godiste unesenog auta je: " << godiste << '\n';
+}
+
+int main() {
+    Auto auto1, auto2;
+
+    auto1.marka = "Ferrari";
+    auto1.model = "La Ferrari";
+    auto1.godiste = 2012;
+
+    cout << "Unesite marku 2. auta!\n";
+    getline(cin, auto2.marka);
+
+    cout << "Unesite model 2. auta!\n";
+    getline(cin, auto2.model);
+
+    cout << "Unesite godiste 2. auta!\n";
+    cin >> auto2.godiste;
+
+    cout << "Ispis podataka 1. auta:\n";
+    auto1.ispisi_marku(auto1.marka);
+    auto1.ispisi_model(auto1.model);
+    auto1.ispisi_godiste(auto1.godiste);
+    
+    cout << endl;
+
+    cout << "Ispis podataka 2. auta:\n";
+    auto2.ispisi_marku(auto2.marka);
+    auto2.ispisi_model(auto2.model);
+    auto2.ispisi_godiste(auto2.godiste);
     return 0;
 }
 /*
