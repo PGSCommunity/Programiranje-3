2f920e8033eb902a9a341acd4fa76bdf1f9aa0bb
Danin Sadžak <daninsadzak@gmail.com>
2023-10-02 19:43:22 +0200

    Testovi - Klase - Postavljen 2. zadatak

---
+++ Testovi/Klase/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati klasu Kvadar, koja sadrzi tri metode a ciji je zadatak izracunati i vratiti vrijednosti povrsine, 
+zapremine i duzine prostorne dijagonale kvadra ( P = 2(ab + ac + bc), V = abc, D^2 = a^2 + b^2 + c^2). Potrebno je
+razdvojiti definicije i deklaracije metoda. U glavnoj funkciji se definira objekt k1 i unose dizne stranica a, b
+i c.
+*/
