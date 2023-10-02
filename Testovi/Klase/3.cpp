ea092979453895bd245b34932f7abce6165877a0
Danin Sadžak <daninsadzak@gmail.com>
2023-10-02 19:49:10 +0200

    Testovi - Klase - Postavljen 3. zadatak

---
+++ Testovi/Klase/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati klasu Racun_Ime. Klasa sadrzi tri metode:
+    a) Unos() - Omogucava unos vrijednosti dva broja x, y
+    b) BrojDjeljivih() - Racuna i ispisuje broj parnih brojeva djeljivih sa 7 u intervalu x-y
+    c) SumaNeparnih() - Od y proizvoljno ucitanih brojeva racuna sumu neparnih, pa vraca u funkciju proracunatu
+    vrijednost uvecanu x puta
+*/
