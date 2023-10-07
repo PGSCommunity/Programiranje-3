3663c22f43345bf8ec4d68682979aca765af328b
Danin Sadžak <daninsadzak@gmail.com>
2023-10-07 19:05:20 +0200

    Vjezbe - Nasljedjivanje - Zavrsen 3. zadatak

--- Vjezbe/Nasljedjivanje/3.cpp
+++ Vjezbe/Nasljedjivanje/3.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Pravougaonik {
+    public:
+        int dA, dB;
+        void unosStranica();
+        int povrsina();
+};
 
+void Pravougaonik::unosStranica() {
+    cout << "Unesite stranice a i b!\n";
+    cin >> dA >> dB;
+}
+
+int Pravougaonik::povrsina() {
+    return dA * dB;
+}
+
+class Kvadrat:public Pravougaonik {
+    public:
+        int povrsina();
+};
+
+int Kvadrat::povrsina() {
+    return pow(dA, 2);
+}
+
+int main() {
+    Pravougaonik p;
+    p.unosStranica();
+    cout << "Povrsina pravougaonika iznosi: " << p.povrsina() << '\n';
+
+    cout << endl;
+
+    Kvadrat k;
+    k.unosStranica();
+    cout << "Povrsina kvadrata iznosi: " << k.povrsina() << '\n';
     return 0;
 }
 
@@ -12,4 +46,6 @@ Pravougaonik kreira se konstruktor koji postavlja duzine stranica na proizvoljne
 koja vraca vrijednosti povrsine pravougaonika. Zatim kreirati klasu Kvadrat koja nasljeduje atribute klase
 Pravougaonik i sadrzi metodu Povrsina, koja vraca vrijednosti povrsine kvadrata (za manju od vrijednosti duzina
 stranica pravougaonika).
+
+Editor's note: Konstruktor je jos jedno ima za metodu.
 */
