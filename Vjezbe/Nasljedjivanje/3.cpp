42e6a60c75648a26efecdfe8ad8bce2ab4d43ec7
Danin Sadžak <daninsadzak@gmail.com>
2023-10-07 18:54:33 +0200

    Vjezbe - Nasljedjivanje - Postavljen 3. zadatak

---
+++ Vjezbe/Nasljedjivanje/3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Pravougaonik koja ima atribute dA i dB koji oznacavaju duzine stranica pravougaonika. Unutar klase
+Pravougaonik kreira se konstruktor koji postavlja duzine stranica na proizvoljne vrijednosti te metoda Povrsina
+koja vraca vrijednosti povrsine pravougaonika. Zatim kreirati klasu Kvadrat koja nasljeduje atribute klase
+Pravougaonik i sadrzi metodu Povrsina, koja vraca vrijednosti povrsine kvadrata (za manju od vrijednosti duzina
+stranica pravougaonika).
+*/
