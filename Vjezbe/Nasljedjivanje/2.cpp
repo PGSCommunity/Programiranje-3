654f1e14ff98a8b8c02579ec222173f7c63e369d
Danin Sadžak <daninsadzak@gmail.com>
2023-10-07 18:48:38 +0200

    Vjezbe - Nasljedjivanje - Postavljen 2. zadatak

---
+++ Vjezbe/Nasljedjivanje/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Zivotinja koja ima metodu "glasanje", te metodu "spavaj" i "jedi". Kreirati klasu Macka koja 
+nasljedjuje klasu Zivotinja te ima atribute ime i vrsta. U glavnom programu instincirati objekt klase Macka pod
+imenom Garfild, pozvati funkciju za glasanje te ispisati ime macke.
+*/
