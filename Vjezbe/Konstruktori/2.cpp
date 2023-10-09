4c31c26513b103a15a2c99ecb46d473f2b9e810f
Danin Sadžak <daninsadzak@gmail.com>
2023-10-10 00:23:14 +0200

    Vjezbe - Konstruktori - Postavljen 2. zadatak

---
+++ Vjezbe/Konstruktori/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Deklarisati klasu Auto sa varijablama brend, model i godiste. Unutar klase treba da se napravi konstruktor koji
+sadrzi vrijednosti deklariranih varijabli, pa da se u glavnoj funkciji deklarisu dva objekta, jedan koji korisnik
+definira po zelji, a drugi treba korisnik treba da unese.
+*/
