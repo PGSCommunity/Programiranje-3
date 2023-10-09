818fc647a449858ee2c22285d4f872c01312bb2d
Danin Sadžak <daninsadzak@gmail.com>
2023-10-10 00:30:53 +0200

    Vjezbe - Konstruktori - Postavljen 3. zadatak

---
+++ Vjezbe/Konstruktori/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Tacka koja u javnom dijelu ima varijable (koordinate) x i y, metodu koja se zove Ishodiste koja
+racuna udaljenost te tacke od ishodista. Klasa ima konstruktor koji postavlja vrijednosit koordinate tacke koji
+postavlja korisnik.
+*/
