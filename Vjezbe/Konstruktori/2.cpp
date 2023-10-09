b13d0245529eb7bf13a23ac750c5ed982b85d4c9
Danin Sadžak <daninsadzak@gmail.com>
2023-10-10 00:28:43 +0200

    Vjezbe - Konstruktori - Zavrsen 2. zadatak

--- Vjezbe/Konstruktori/2.cpp
+++ Vjezbe/Konstruktori/2.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+class Auto {
+    public:
+        string brend;
+        string model;
+        int godiste;
+        Auto(string x, string y, int z);
+};
 
+Auto::Auto(string x, string y, int z) {
+    brend = x;
+    model = y;
+    godiste = z;
+}
+
+int main() {
+    string brend, model;
+    int godiste;
+
+    cout << "Unesite brend auta!\n";
+    getline(cin, brend);
+
+    cout << "Unesite model auta!\n";
+    getline(cin, model);
+
+    cout << "Unesite godiste auta!\n";
+    cin >> godiste;
+
+    Auto a1("Ferrari", "LaFerrari", 2016), a2(brend, model, godiste);
+
+    cout << "Ispis podataka 1. auta:\n";
+    cout << "Brend: " << a1.brend << '\n';
+    cout << "Model: " << a1.model << '\n';
+    cout << "Godiste: " << a1.godiste << '\n';
+    cout << endl;
+
+    cout << "Ispis podataka 2. auta:\n";
+    cout << "Brend: " << a2.brend << '\n';
+    cout << "Model: " << a2.model << '\n';
+    cout << "Godiste: " << a2.godiste << '\n';
+    
     return 0;
 }
 /*
