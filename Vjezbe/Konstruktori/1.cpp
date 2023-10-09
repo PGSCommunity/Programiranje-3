a19f7d4bd8e3d76e19f29b801949b6f3bcc86c7a
Danin Sadžak <daninsadzak@gmail.com>
2023-10-10 00:16:36 +0200

    Vjezbe - Konstruktori - Postavljen 1. zadatak

---
+++ Vjezbe/Konstruktori/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Deklarisati klasu "Pravougaonik", sa varijablama a, b, i metodama Povrsina i Obim. Klasa ima konstruktor koji
+omogucava unos varijabli a i b. U main funkciji se deklarise objekat i pozivaju metode.
+*/
