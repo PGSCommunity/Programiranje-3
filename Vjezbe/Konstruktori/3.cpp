b9ff1a82b9232cd98e5404f8e7cbbfffb130ecc1
Danin Sadžak <daninsadzak@gmail.com>
2023-10-10 00:34:30 +0200

    Vjezbe - Konstruktori - Zavrsen 3. zadatak

--- Vjezbe/Konstruktori/3.cpp
+++ Vjezbe/Konstruktori/3.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Tacka {
+    public:
+        int x, y;
+        Tacka(int a, int b);
+        float Ishodiste();
+};
 
+Tacka::Tacka(int a, int b) {
+    x = a;
+    y = b;
+}
+
+float Tacka::Ishodiste() {
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+int main() {
+    int a, b;
+    cout << "Unesite koordinatne tacke!\n";
+    cin >> a >> b;
+
+    Tacka t(a, b);
+    cout << "Udaljenost tacke od ishodista: " << t.Ishodiste() << '\n';
     return 0;
 }
 
