b7b13db08c5b741d5ef05aaa6eb669e85eaaddb9
Danin Sadžak <daninsadzak@gmail.com>
2023-10-10 00:20:17 +0200

    Vjezbe - Konstruktori - Zavrsen 1. zadatak

--- Vjezbe/Konstruktori/1.cpp
+++ Vjezbe/Konstruktori/1.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Pravougaonik {
+    public:
+        int a, b;
+        Pravougaonik();
+        int Povrsina();
+        int Obim();
+};
 
+Pravougaonik::Pravougaonik() {
+    cout << "Unesite vrijednosti a i b!\n";
+    cin >> a >> b;
+}
+
+int Pravougaonik::Povrsina() {
+    return a * b;
+}
+
+int Pravougaonik::Obim() {
+    return 2 * (a + b);
+}
+
+int main() {
+    Pravougaonik p;
+    cout << "Povrsina iznosi: " << p.Povrsina() << '\n';
+    cout << "Obim iznosi: " << p.Obim() << '\n';
     return 0;
 }
 
 /*
 Deklarisati klasu "Pravougaonik", sa varijablama a, b, i metodama Povrsina i Obim. Klasa ima konstruktor koji
-omogucava unos varijabli a i b. U main funkciji se deklarise objekat i pozivaju metode.
+omogucava unos varijabli a i b. U main funkciji se deklarise objekat i ispisuju metode.
 */
