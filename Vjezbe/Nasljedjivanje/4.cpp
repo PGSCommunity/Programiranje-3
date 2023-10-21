421845afd77fc69672cf0d8c65322755e2fc72d1
Danin Sadžak <daninsadzak@gmail.com>
2023-10-21 10:23:41 +0200

    Vjezbe - Nasljedjivanje - Zavrsen 4. zadatak

--- Vjezbe/Nasljedjivanje/4.cpp
+++ Vjezbe/Nasljedjivanje/4.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Ucenik {
+    protected:
+        string ime, odjeljenje;
+    public:
+        Ucenik(string i, string o);
+        void Ispisi();
+};
 
+Ucenik::Ucenik(string i, string o) {
+    ime = i;
+    odjeljenje = o;
+}
+
+void Ucenik::Ispisi() {
+    cout << "Ucenik " << ime << " je u odjeljenju " << odjeljenje << '\n';
+}
+
+class Maturant:public Ucenik {
+    protected:
+        string predmet_maturskog;
+    public:
+        Maturant(string ime, string odjeljenje, string pred_mat):Ucenik(ime, odjeljenje) {
+            predmet_maturskog = pred_mat;
+        }
+        void Ispisi();
+};
+
+void Maturant::Ispisi() {
+    cout << "Ucenik " << ime << " je u odjeljenju " << odjeljenje << " i radi maturski rad iz predmeta " << predmet_maturskog << '\n';
+}
+
+int main() {
+    Ucenik u1("Mickey Mouse", "III-f");
+    u1.Ispisi();
+
+    Maturant u2("Paja Patak", "IV-f", "Programiranje");
+    u2.Ispisi();
     return 0;
 }
 
