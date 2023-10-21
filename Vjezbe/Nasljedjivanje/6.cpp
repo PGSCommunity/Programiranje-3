a0b2af3b658e3f199fa0e2e29a124d6abfc94428
Danin Sadžak <daninsadzak@gmail.com>
2023-10-21 15:54:38 +0200

    Vjezbe - Nasljedjivanje - Postavljen 6. zadatak

---
+++ Vjezbe/Nasljedjivanje/6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati kasu Vozilo koja ima staticki atribut koji oznacava najvecu mogucu prijedjenu kilometrazu. Klasa ima
+konstruktor, i metodu koja ispisuje zaglavlje za tu klasu. Kreirati klasu SportskoAuto koja nasljeduje klasu 
+Vozilo te posjeduje parametre naziv i prosjecna_brzina. Napraviti metodu koja ce izracunavati koliko vremena treba
+proci da bi automobil dostigao najvecu mogucu prijedjenu kilometrazu ako stalno vozi zadanom prosjecnom brzinom.
+*/
