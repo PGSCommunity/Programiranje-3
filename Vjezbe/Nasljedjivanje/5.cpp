1ee620f8742b0636d9a0c28a8ac5828a78952e7d
Danin Sadžak <daninsadzak@gmail.com>
2023-10-21 15:44:37 +0200

    Vjezbe - Nasljedjivanje - Zavrsen 5. zadatak

--- Vjezbe/Nasljedjivanje/5.cpp
+++ Vjezbe/Nasljedjivanje/5.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class GeometrijskiLik {
+    protected:
+        string naziv;
+    public:
+        void Ispisi();
+};
 
+void GeometrijskiLik::Ispisi() {
+    cout << "Ime geometrisjkog lika je: " << naziv << '\n';
+}
+
+class Krug:public GeometrijskiLik {
+    protected:
+        const double pi = 3.14;
+        double r;
+    public:
+        Krug(double r);
+        double IzracunajPovrsinu();
+        double IzracunajObim();
+        void Ispisi();
+};
+
+Krug::Krug(double r) {
+    this->r = r;
+    naziv = "Krug!";
+}
+
+double Krug::IzracunajPovrsinu() {
+    return pow(r, 2) * pi;
+}
+
+double Krug::IzracunajObim() {
+    return 2 * r * pi;
+}
+
+void Krug::Ispisi() {
+    GeometrijskiLik::Ispisi();
+    cout << "Povrsina kruga iznosi: " << IzracunajPovrsinu() << ",\na obim kruga iznosi: " << IzracunajObim() << '\n';
+}
+
+int main() {
+    GeometrijskiLik g;
+    g.Ispisi();
+
+    Krug k(5);
+    k.Ispisi();
     return 0;
 }
 
