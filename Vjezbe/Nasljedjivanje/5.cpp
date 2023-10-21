c16e2fe5ebc63dc58dca39ac83d744ae51cf36e0
Danin Sadžak <daninsadzak@gmail.com>
2023-10-21 15:38:37 +0200

    Vjezbe - Nasljedjivanje - Postavljen 5. zadatak

---
+++ Vjezbe/Nasljedjivanje/5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Napraviti klasu geometrijski_lik sa zasticenom varijablom naziv. U javnom dijelu ima metodu Ispisi() koja ispisuje
+naziv geometrijskog lika. Napraviti klasu Krug koja nasljeduje klasu geometrijski_lik sa zasticenim varijablama
+pi i r, a u javnom dijelu ima konstruktor kojim se dodjeljuje vrijendost poluprecnika i naziv iz klase geometrijski
+lik. Takodjer ima tri metoda, IzracunajPovrsinu() koja vraca vrijednosti povrsine kruga, IzracunajObim() koji vraca
+vrijednosti obima kruga, i Ispisi() u kojoj se ispisuje naziv geometrijskog lika iz klase roditelja, i ispis povrsine
+i obima kruga.
+*/
