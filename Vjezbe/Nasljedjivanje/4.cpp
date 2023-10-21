8c824f8aa8d8df52a85e0b3087f6e1bbd6384b17
Danin Sadžak <daninsadzak@gmail.com>
2023-10-21 10:16:42 +0200

    Vjezbe - Nasljedjivanje - Postavljen 4. zadatak

---
+++ Vjezbe/Nasljedjivanje/4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Napraviti klasu Ucenik sa vrijednostima ime i odjeljenje koji su zasticeni. U javnom dijelu napraviti konstruktor
+kojem se dijele vrijednosti i metodu Ispisi() koja ispisuje vrijednosti klase. Zatim napraviti klasu Maturant
+koji nasljedjuje klasu Ucenik, a ima dodatnu zasticenu varijablu predmet_maturskog. Treba napraviti konstruktor
+u Maturant klasi koja nasljedjuje vrijednosti Ucenik konstruktora, a jos se treba dodijeliti i vrijednost varijable
+predmet_maturksog. U glavnoj funkciji treba napraviti objekte i pozivati metode.
+*/
