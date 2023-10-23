1c8f96a67e11d41210e4d9442c5e5bc3cb2a40e0
Danin Sadžak <daninsadzak@gmail.com>
2023-10-23 10:32:24 +0200

    Vjezbe - Nasljedjivanje - Postavljen 8. zadatak

---
+++ Vjezbe/Nasljedjivanje/8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Knjiga (posjeduje atribute naziv, broj stranica, autor) i podklasu Udzbenik (posjeduje dodatni 
+atribut - predmet). Omoguciti korisnicki unos atributa klase Uzbenik, te ukoliko je unesen naziv udžbenika 
+Programiranje, ispisati ime autora udzbenika. 
+*/
