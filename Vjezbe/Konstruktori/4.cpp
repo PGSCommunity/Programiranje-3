54ea9db17480b0dc2677d088614e41ee4a4aad94
Danin Sadžak <daninsadzak@gmail.com>
2023-10-23 11:01:30 +0200

    Vjezbe - Konstruktori - Postavljen 4. zadatak

--- Vjezbe/Konstruktori/4.cpp
+++ Vjezbe/Konstruktori/4.cpp
@@ -11,14 +11,14 @@ Napisati klasu Pravougaonik koja sadrži:
 Atribute a i b tipa double koji predstavljaju dužine stranica pravougaonika
 Defaultni konstruktor koji postavlja vrijednosti stranica na 1 ukoliko korisnik kreira Pravougaonik a ne 
 proslijedi vrijednosti stranica
-Konstruktor koji prima dva parametra A i B tipa double, koji postavlja atribute a i b na vrijednosti A i B redom 
-ukoliko su A i B pozitivni. Ukoliko je bar neki od njih negativan, korisniku treba ispisati da proslijeđeni 
+Konstruktor koji prima dva parametra A i B tipa double, koji postavlja atribute a i b na vrijednosti A i B redom
+ukoliko su A i B pozitivni. Ukoliko je bar neki od njih negativan, korisniku treba ispisati da proslijeđeni
 parametri nisu validni.
-Konstruktor koji prima jedan parametar A tipa double, koji postavlja oba atributa a i b na vrijednost A. U suštini
-, ovaj konstruktor služi za lakše kreiranje kvadrata.
+Konstruktor koji prima jedan parametar A tipa double, koji postavlja oba atributa a i b na vrijednost A. 
+U suštini, ovaj konstruktor služi za lakše kreiranje kvadrata.
 Funkciju obim unutar klase (metoda klase) koja vraća obim pravougaonika nad kojim se pozove
 Funkciju povrsina unutar klase (metoda klase) koja vraća površinu pravougaonika nad kojim se pozove
 Funkciju dijagonala koja vraća dužinu dijagonale pravougaonika	
-Funkciju ispisi unutar klase koja ispisuje korisniku stranice pravougaonika, njegov obim i površinu, te 
-dijagonalu, u odgovarajućem formatu.
+Funkciju ispisi unutar klase koja ispisuje korisniku stranice pravougaonika, njegov obim i površinu, 
+te dijagonalu, u odgovarajućem formatu.
 */
