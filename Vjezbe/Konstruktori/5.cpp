cbf3a0c90ea86e7a48fcb64083ca78f7103337ee
Danin Sadžak <daninsadzak@gmail.com>
2023-10-23 11:15:27 +0200

    Vjezbe - Konstruktori - Postavljen 5. zadatak

---
+++ Vjezbe/Konstruktori/5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+
+/*
+Napisati klasu Tacka koja sadrži: 
+Atribute x i y tipa double koji predstavljaju koordinate tačke
+Defaultni konstruktor koji postavlja vrijednosti koordinata na 0 kada ih korisnik ne proslijedi
+Konstruktor koji prima dva parametra X i Y i postavlja koordinate x i y na vrijednosti X i Y, ovim redoslijedom
+Funkciju Udaljenost koja se nalazi unutar klase (dakle radi se o metodi klase) koja prima jedan parametar tipa 
+Tacka. Funkcija treba da vrati udaljenost između tačke nad kojom je pozvana funkcija sa tačkom koja je 
+proslijeđena kao parametar
+Funkciju Udaljenost koja se nalazi van klase i koja prima dva parametra, oba tipa Tacka, koja vraća udaljenost 
+između dvije proslijeđene tačke.
+*/
