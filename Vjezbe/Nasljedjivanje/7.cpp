06d4898c9bef8e9de2bef125fc2799a764c1f96b
Danin Sadžak <daninsadzak@gmail.com>
2023-10-23 10:11:05 +0200

    Vjezbe - Nasljedjivanje - Postavljen 7. zadatak

---
+++ Vjezbe/Nasljedjivanje/7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Artikal koja kao atribote ima cijenu, naziv i šifru artikla te atribut PDV koji ji 17%.
+Zatim kreirati izvedene klase Hrana i Pice koje imaju posebne artikle;
+Hrana ima hranjivu vrijednost u kalorijama i težinu a piæe ima zapreminu u litrima.
+U klasi artikal definiraju se metode IzracunajPDV i metoda za ispis naziva artikla.
+U glavnom programu instancira se nekoliko objekata hrane i pica.
+Na kraju je portrebno ispisati naziv, PDV artikla na zadanu cijenu i kojoj skupini pripada (hrana ili pice).
+*/
