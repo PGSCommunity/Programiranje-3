47a813c91346bb79248a89fe2fa6df29663f6b8c
Danin Sadžak <daninsadzak@gmail.com>
2023-10-23 11:13:56 +0200

    Vjezbe - Konstruktori - Zavrsen 4. zadatak

--- Vjezbe/Konstruktori/4.cpp
+++ Vjezbe/Konstruktori/4.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Pravougaonik {
+    protected:
+        double a, b;
+    public:
+        Pravougaonik();
+        Pravougaonik(double A, double B);
+        Pravougaonik(double A);
+        double Obim();
+        double Povrisna();
+        double Dijagonala();
+        void Ispisi();
+};
 
+Pravougaonik::Pravougaonik() {
+    a = 1; 
+    b = 1;
+}
+
+Pravougaonik::Pravougaonik(double A, double B) {
+    if (A >= 0 || B >= 0) {
+        a = A;
+        b = B;
+    } else {
+        cout << "Jedan ili obadva parametra je negativan, parametri nisu validni.\n";
+    }
+}
+
+Pravougaonik::Pravougaonik(double A) {
+    if (A >= 0) a = A;
+    else cout << "Vrijednost A je negativna. Parametar nije validan.\n";
+}
+
+double Pravougaonik::Obim() {
+    return 2 * (a + b);
+}
+
+double Pravougaonik::Povrisna() {
+    return a * b;
+}
+
+double Pravougaonik::Dijagonala() {
+    return sqrt(a * a + b * b);
+}
+
+void Pravougaonik::Ispisi() {
+    cout << "Ispis vrijednosti pravougaonika:\n\n";
+    cout << "Povrsina: " << Povrisna() << '\n';
+    cout << "Obim: " << Obim() << '\n';
+    cout << "DIjagonala: " << Dijagonala() << '\n';
+}
+
+int main() {
+    Pravougaonik pravougaonik1;
+    Pravougaonik pravougaonik2(3, 4);
+    Pravougaonik pravougaonik3(5);
+
+    pravougaonik1.Ispisi();
+    cout << endl;
+    pravougaonik2.Ispisi();
+    cout << endl;
+    pravougaonik3.Ispisi();
     return 0;
 }
 
