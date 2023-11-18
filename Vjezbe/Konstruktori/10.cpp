3a416066573d23b00701c40e3a9d6e0c70c70cbe
Danin Sadžak <daninsadzak@gmail.com>
2023-11-18 17:09:07 +0100

    Vjezbe - Konstruktori - Postavljen 10. zadatak

---
+++ Vjezbe/Konstruktori/10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Izraditi programsko rješenje uz primjenu klase koja će pretvarati visinu u cm u stope. Klasa treba
+imati dva konstruktora, jedan bez i jedan s parametrima. U konstruktoru bez parametara visina
+treba biti 100. U klasi trebamo imati jednu funkciju tipa public pretvorba() te člansku varijablu
+tipa private h. Kreirati dva objekta klase jedan bez prosljeđivanja parametara, i jedan s
+prosljeđivanjem te iz maina za njih ispisati vrijednost visinu u stopama s preciznošću od dvije
+decimale pozivanjem pripadajuće metode.
+
+Podsjetnik: 100 cm = 3.28084 ft
+*/
