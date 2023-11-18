e9b06f22fb18a26cbaa4108a38a91338c64025d1
Danin Sadžak <daninsadzak@gmail.com>
2023-11-18 16:01:34 +0100

    Vjezbe - Konstruktori - Postavljen 6. zadatak

---
+++ Vjezbe/Konstruktori/6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Napisati program sa deklaracijom klase Kocka.
+Klasa treba imati:
+- jednu clansku varijablu (a) s pravom pristupa private
+- konstruktor
+- destruktor
+- funkciju povrsina koja ce racunati povrsinu kocke 
+- funkciju zapremina koja ce racunati zapreminu kocke
+*/
