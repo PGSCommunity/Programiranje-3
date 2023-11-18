1619bfb0821236f757ec4fe16ef81e516db84d4e
Danin Sadžak <daninsadzak@gmail.com>
2023-11-18 16:13:45 +0100

    Vjezbe - Konstruktori - Postavljen 7. zadatak

---
+++ Vjezbe/Konstruktori/7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+/*
+Deklarisati klasu Valjak.
+
+Klasa treba imati:
+- dvije clanske varijable (radijus i visinu) s pravom pristupa private.
+- funkciju Zapremina bez parametara s pravom pristupa public koja kao rezultat vraca zapreminu.
+
+U glavnoj funkciji treba:
+- deklarisati dva objekta klase valjak
+- pristupiti clanskim varijablama preko funkcije Zapremina i dodijeliti im neke vrijednosti
+- izracunati zapreminu oba valjka pozivanjem funkcije i spremiti ih u neke varijable
+- ispisati obje zapremine
+*/
