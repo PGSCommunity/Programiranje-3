6d589cd1dae3d99385e1018a83022933366f0df2
Danin Sadžak <daninsadzak@gmail.com>
2023-11-18 16:06:00 +0100

    Vjezbe - Konstruktori - Postavljen 6. zadatak

--- Vjezbe/Konstruktori/6.cpp
+++ Vjezbe/Konstruktori/6.cpp
@@ -14,4 +14,7 @@ Klasa treba imati:
 - destruktor
 - funkciju povrsina koja ce racunati povrsinu kocke 
 - funkciju zapremina koja ce racunati zapreminu kocke
+
+U mainu treba:
+- deklarisati dva objekta klase kocka, ispisati povrsinu i zapreminu pozivanjem pripadajucih funkcija
 */
