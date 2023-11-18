b918649fc54271bc7b2e508423f9fc8f08d16e9d
Danin Sadžak <daninsadzak@gmail.com>
2023-11-18 16:46:25 +0100

    Vjezbe - Konstruktori - Postavljen 8. zadatak

---
+++ Vjezbe/Konstruktori/8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Za pravilnu šesterostranu prizmu izraditi programsko rješenje uz primjenu klase koja će imati dva
+konstruktora, jedan bez i jedan s parametrima. U klasi trebamo imati dvije funkcije tipa public
+povrsina() i zapremina() te dvije članske varijable tipa private a i h. Kreirati dva objekta klase
+jedan s i jedan bez prosljeđivanja parametara te iz maina za oba objekta ispisati vrijednost
+površine i zapremine s preciznošću od dvije decimale pozivanjem pripadajućih metoda.
+*/
