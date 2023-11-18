ba319aea7df3e0d801ddcc88cd19e8be76bc8793
Danin Sadžak <daninsadzak@gmail.com>
2023-11-18 17:01:58 +0100

    Vjezbe - Konstruktori - Postavljen 9. zadatak

---
+++ Vjezbe/Konstruktori/9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Izraditi programsko rješenje uz primjenu klase koja će računati prosječnu potrošnju goriva automobila na
+100 km. Klasa imati dva konstruktora, jedan bez i jedan s parametrima. U klasi trebamo imati funkcije tipa
+public potrosnja() te dvije članske varijable tipa private prijedjeniPut i velicinaRezervoara. Kreirati tri
+objekta klase jedan bez prosljeđivanja parametara, te dva s prosljeđivanjem te iz maina za njih ispisati
+vrijednost prosječne potrošnje goriva preciznošću od dvije decimale pozivanjem pripadajućih metoda.
+Podsjetnik:
+
+prosjecnaPotrosnja=velicinaSpremnika*100/prijedjeniPut
+*/
