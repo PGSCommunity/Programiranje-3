5980ed7a11d1dcc218410d46c6ecfe2258a0a8a2
Danin Sadžak <daninsadzak@gmail.com>
2023-11-19 21:49:29 +0100

    Vjezbe - Konstruktori - Zavrsen 11. zadatak

--- Vjezbe/Konstruktori/11.cpp
+++ Vjezbe/Konstruktori/11.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+class Student {
+    protected:
+        string ime, prezime, email, datumRodjenja;
+    public:
+        Student(string x, string y, string z, string q) {
+            ime = x;
+            prezime = y;
+            email = z;
+            datumRodjenja = q;
+        }
 
+        int ciklusStudija();
+        int godinaStudija();
+        void Ispisi();
+        bool provjeriDatum();
+};
+
+int Student::ciklusStudija() {
+    // Varijabla datumRodjena = dd/mm/gggg
+    int godina = 2023;
+    string uzmiGodinu = datumRodjenja.substr(6, 4);
+    int pretvoriGodinuUInt = stoi(uzmiGodinu);
+
+    int godinaStudenta = godina - pretvoriGodinuUInt;
+    int ciklus{0};
+
+    if (godinaStudenta >= 23 && godinaStudenta < 26) ciklus = 1;
+    else if (godinaStudenta == 26) ciklus = 2;
+    else if (godinaStudenta >= 27) ciklus = 3;
+    else ciklus = 0;
+
+    return ciklus;
+}
+
+int Student::godinaStudija() {
+    int godina = 2023;
+    string uzmiGodinu = datumRodjenja.substr(6, 4);
+    int pretvoriGodinuUInt = stoi(uzmiGodinu);
+
+    int godinaStudenta = godina - pretvoriGodinuUInt;
+    int godinaStudija{0};
+
+    // Ovo je uzas.
+    if (godinaStudenta == 19) godinaStudija = 1;
+    if (godinaStudenta == 20) godinaStudija = 2;
+    if (godinaStudenta == 21) godinaStudija = 3;
+    if (godinaStudenta == 23) godinaStudija = 4;
+    if (godinaStudenta == 24) godinaStudija = 5;
+    if (godinaStudenta == 25) godinaStudija = 6;
+    if (godinaStudenta == 26) godinaStudija = 7;
+    if (godinaStudenta >= 27) godinaStudija = 8;
+
+    return godinaStudija;
+}
+
+void Student::Ispisi() {
+    cout << "IME STUDENTA: " << ime << '\n'; 
+    cout << "PREZIME STUDENTA: " << prezime << '\n'; 
+    cout << "EMAIL STUDENTA: " << email << '\n'; 
+    cout << "DATUM RODJENJA STUDENTA: " << datumRodjenja << '\n'; 
+}
+
+bool Student::provjeriDatum() {
+    return (datumRodjenja.size() == 10) ? true : false;
+}
+
+int main() {
+    Student s("Danin", "Sadzak", "sadzake@proton.me", "08/02/1998");
+    cout << "CIKLUS STUDIJA STUDENTA: " << s.ciklusStudija() << '\n';
+    cout << "GODINA STUDIJA STUDENTA: " << s.godinaStudija() << '\n';
+    s.Ispisi();
+    cout << s.provjeriDatum() << '\n';
     return 0;
 }
 
