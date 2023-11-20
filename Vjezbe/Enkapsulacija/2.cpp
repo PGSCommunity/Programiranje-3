7226f0aeb651cd23957f4f7f8528d139f3ba93de
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 19:09:39 +0100

    Vjezbe - Enkapsulacija - Postavljen 2. zadatak

---
+++ Vjezbe/Enkapsulacija/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Deklarisati klasu Pravougaonik sa privatnim varijablama a i b. U public dijelu deklarisati dvije metode, jedan
+setter, koji uzima varijable iz private dijela, i getPovrisna koja funkcionise kao getter, i vraca povrsinu 
+pravougaonika. 
+*/
