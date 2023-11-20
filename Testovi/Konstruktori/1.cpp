f7b236cf7204228dba50b8dfecec001c2c2a93e2
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 18:42:41 +0100

    Testovi - Konstruktori - Postavljen 1. zadatak

---
+++ Testovi/Konstruktori/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Gospođa Jagoda otvara slastičarnu. Mjesečni najam prostora je košta 700KM (postavi kao globalnu konstantu).
+a. Napiši klasu zarada koja će imati private varijablu cijenaPoKolacu.
+
+b. funkcija prodajaKolaca treba vratiti u main koliko kolača će morati prodati dnevno (sama unosi prosječnu 
+cijenu kolača u KM iz maina preko konstruktora) da bi pokrila trošak najma. Ispis treba biti iz maina zaokružen
+na prvi veći cijeli broj kolača upotrebom prikladne funkcije.
+
+c. osim kolača gospođa Jagoda planira prodavati i sladolede čija je cijena 6KM/kugli (postavi kao globalnu 
+konstantu). Funkcija klase sladoled treba u main vratiti koliko kugli sladodela treba prodati dnevno, da bi 
+zaradila za svoju platu i doprinose koji mjesečno iznose 2400 KM (postavi kao globalnu konstantu). Ispis treba
+biti iz maina zaokružen na prvi veći cijeli broj upotrebom prikladne funkcije.
+
+d. Ne zaboravi kreirati konstruktor i destruktor!
+*/
