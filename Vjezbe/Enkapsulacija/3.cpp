d60a10d99a17ace46efdef88cf4a6b442364ca98
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 19:14:58 +0100

    Vjezbe - Enkapsulacija - Postavljen 3. zadatak

---
+++ Vjezbe/Enkapsulacija/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Deklarisati klasu kvadrat koji ima privatnu varijablu a. U glavnom dijelu, prave se tri metode. Prva metoda je 
+setter koji uzima stranicu kvadrata, a druge dvije funkcionisu kao getter. Jedna getPovrsina koja vraca povrsinu
+kvadrata, a druga getObim koja vraca povrsinu obima kvadrata. U main funkciji korisnik treba da sam unese stranicu
+a. 
+*/
