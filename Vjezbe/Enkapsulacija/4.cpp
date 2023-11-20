019cb44f4a307c22c597b403b1a6d4a861634fc8
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 19:20:13 +0100

    Vjezbe - Enkapsulacija - Postavljen 4. zadatak

---
+++ Vjezbe/Enkapsulacija/4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Deklarisati klasu Lopta, koja sadrzi privatnu varijablu precnik. U public dijelu treba da ima tri metode, jedan
+setter, i getteri getPovrsina i getZapremina, koji vracaju povrsinu i zapreminu lopte. U main funkciji korisnik
+treba da unese precnik lopte. Sve konstante se trebaju deklarirati u public dijelu klase.
+*/
