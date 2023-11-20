fee0cf6407b0be8c9153f7dea1d86f3c4d705f99
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 18:52:10 +0100

    Testovi - Konstruktori - Zavrsen 1. zadatak

--- Testovi/Konstruktori/1.cpp
+++ Testovi/Konstruktori/1.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+const float Najam = 700;
+const float cijenaKugleSladoleda = 6;
+const float placaDoprinosi = 2400;
 
+class Zarada {
+    private:
+        float cijenaPoKolacu;
+    public:
+        Zarada(float prosjecnaCijenaKolaca) {
+            cijenaPoKolacu = prosjecnaCijenaKolaca;
+        }
+        float prodajaKolaca();
+        ~Zarada() {}
+};
+
+float Zarada::prodajaKolaca() {
+    return ceil(Najam / cijenaPoKolacu);
+}
+
+class Sladoled {
+    public:
+        float prodajaSladoleda();
+};
+
+float Sladoled::prodajaSladoleda() {
+    return ceil(placaDoprinosi / cijenaKugleSladoleda);
+}
+
+int main() {
+    float 
+    prosjecnaCijenaKolaca;
+    cout << "Unesi cijenu jednog kolaca: ";
+    cin >> prosjecnaCijenaKolaca;
+    Zarada z(prosjecnaCijenaKolaca);
+    Sladoled s;
+    cout << "Da bi se pokrio mjesecni najam potrebno je dnevno prodati najmanje " << z.prodajaKolaca() << " kolaca po prosjecnoj cijeni " << prosjecnaCijenaKolaca << "KM.\n";
+    cout << "Da bi ste zaradili platu i doprinose morate prodati " << s.prodajaSladoleda() << " kugli slaodleda dnevno.\n";
     return 0;
 }
 
