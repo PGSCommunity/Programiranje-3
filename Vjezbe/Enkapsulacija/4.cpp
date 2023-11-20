cbde049ed3f702976a212d83cb4e59f8def8a349
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 19:24:13 +0100

    Vjezbe - Enkapsulacija - Zavrsen 4. zadatak

--- Vjezbe/Enkapsulacija/4.cpp
+++ Vjezbe/Enkapsulacija/4.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+class Lopta {
+    private:
+        float R;
+    public:
+        const float PI = 3.14;
+        void setPoluprecnik(float x) {
+            R = x;
+        }
+        float getPovrsina() {
+            return 4 * PI * pow(R, 2);
+        }
+        float getZapremina() {
+            return (4 * PI * pow(R, 3)) / 3;
+        }
+};
 
+int main() {
+    float R;
+    cout << "Unesite poluprecnik lopte!\n";
+    cin >> R;
+
+    Lopta l;
+    l.setPoluprecnik(R);
+    cout << "Povrsina lopte iznosi: " << l.getPovrsina() << '\n';
+    cout << "Zapremina lopte iznosi: " << l.getZapremina() << '\n';
     return 0;
 }
 
 /*
-Deklarisati klasu Lopta, koja sadrzi privatnu varijablu precnik. U public dijelu treba da ima tri metode, jedan
+Deklarisati klasu Lopta, koja sadrzi privatnu varijablu poluprecnik. U public dijelu treba da ima tri metode, jedan
 setter, i getteri getPovrsina i getZapremina, koji vracaju povrsinu i zapreminu lopte. U main funkciji korisnik
-treba da unese precnik lopte. Sve konstante se trebaju deklarirati u public dijelu klase.
+treba da unese poluprecnik lopte. Sve konstante se trebaju deklarirati u public dijelu klase.
 */
