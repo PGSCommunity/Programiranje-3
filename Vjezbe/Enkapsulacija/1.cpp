975f036d023889403406cdcfc20db8195f42fd5e
Danin Sadžak <daninsadzak@gmail.com>
2023-11-20 19:04:40 +0100

    Vjezbe - Enkapsulacija - Postavljen 1. zadatak

---
+++ Vjezbe/Enkapsulacija/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Napisati klasu Zaposlenik, sa privatnom varijablom plata, tipa double. U public dijelu, deklarirati tri metode,
+jedan setter, koji ce uzeti vrijednosti plate, getter koji ce vratiti platu, i bonus u kojoj je plata uvecana
+2 puta.
+*/
