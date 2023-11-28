315354bcadaabd4d8b6fc87c0c1637257ddc8670
Danin Sadžak <daninsadzak@gmail.com>
2023-11-28 13:32:38 +0100

    Vjezbe - Kompozicija - Postavljen 2. zadatak

---
+++ Vjezbe/Kompozicija/2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Koordinate (atributi – 2 cjelobrojne vrijednosti, konstruktor, destruktor, metoda koja ispisuje 
+zaglavlje).Potom kreirati izvedenu klasu Kvadrant koja nasljeđuje metode i atribude bazne klase Koordinate, 
+te sadrži konstruktor, destruktor, atribut 'razdaljina' , metodu zaglavlje() (ispisuje poruku 
+„Kvadrant u kojem se nalazi tacka sa koordinatama ... je:  ...  „) , metodu polozaj()
+( ispisuje u kojem kvadrantu se nalazi tačka za učitane koordinate), metodu udaljenost() 
+(računa i ispisuje udaljenost tacke od ishodišta koordinatnog sistema).
+*/
