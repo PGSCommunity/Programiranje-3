9389cd6e2208271633194d30d653017f6b22b76f
Danin Sadžak <daninsadzak@gmail.com>
2023-11-28 14:31:19 +0100

    Vjezbe - Kompozicija - Postavljen 3. zadatak

---
+++ Vjezbe/Kompozicija/3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Vrijeme koja će predstavljati vrijeme u satima, minutama i sekundam.  Iz date klase izvesti klasu 
+Let koja sadrži naziv i broj leta. Podaci nasljeđeni iz osnovne klase predstavljaju vrijeme polaska leta. 
+Ova klasa sadrži funkciju koja omogućava promjenu  ovog vremena za neko kašnjenje (dato u sekundama). 
+U glavnoj funkciji se zadaje vrijeme polaska nekog leta, naziv i broj, te zadaje kašnjenje, a ispisuje novo 
+vrijeme polaska sa uračunatim kašnjenjem.
+*/
