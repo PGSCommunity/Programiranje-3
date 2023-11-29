4549115f11f9c2be4a04092b9c28f01f873cd85f
Danin Sadžak <daninsadzak@gmail.com>
2023-11-29 13:01:03 +0100

    Vjezbe - Kompozicija - Postavljen 5. zadatak

---
+++ Vjezbe/Kompozicija/5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Koristeći OOP potrebno je napisati program koji će izračunati prosječnu temperaturu jednog grada za n mjeseci. 
+Klasa Temperatura pomocu atributa i metoda omogucava sumiranje svih n vrijednosti temperatura uneseneih u 
+glavnoj funkciji. Izvedena klasa Prosjek posjeduje atribute i metode koji ce ispisati naziv grada 
+(unesen u glavnoj funkciji) i prosječnu temperaturu.
+*/
