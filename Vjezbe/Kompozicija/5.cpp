07d645cde532c1587e2d0c207b3ffec6b79359f7
Danin Sadžak <daninsadzak@gmail.com>
2023-11-29 13:23:05 +0100

    Vjezbe - Kompozicija - Zavrsen 5. zadatak

--- Vjezbe/Kompozicija/5.cpp
+++ Vjezbe/Kompozicija/5.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+class Temperatura {
+    protected:
+        double sumaTemperature;
+        int brojMjeseci;
+    public:
+        Temperatura() {
+            sumaTemperature = 0;
+            brojMjeseci = 0;
+        }
 
+        void izracunajTemperaturu(double temperatura) {
+            sumaTemperature += temperatura;
+            brojMjeseci++;
+        }
+
+        double izracunajProsjek() {
+            if (brojMjeseci == 0) return -1;
+            return sumaTemperature / brojMjeseci;
+        }
+
+        virtual void Ispis() {
+            cout << "Prosjecna temperatura u gradu za " << brojMjeseci << " mjeseci je: " << izracunajProsjek() << '\n';
+        }
+
+        void Izvjestaj() {
+            Ispis();
+        }
+
+        ~Temperatura() {}
+};
+
+class Prosjek: public Temperatura {
+    protected:
+        string nazivGrada;
+    public:
+        Prosjek(string naziv) {
+            nazivGrada = naziv;
+        }
+
+        void Ispis() {
+            cout << "Prosjecna temperatura u gradu " << nazivGrada << " za " << brojMjeseci << "mjeseci je: " << izracunajProsjek() << '\n';
+        }
+};
+
+int main() {
+    string grad;
+    int n;
+    cout << "Unesite broj mjeseci!\n";
+    cin >> n;
+
+    cin.ignore();
+
+    cout << "Unesite ime grada!\n";
+    getline(cin, grad);
+
+    Temperatura t;
+    Prosjek p(grad);
+    for(int i = 0; i < n; i++) {
+        double temperatura;
+        cout << "Unesite temperaturu za " << i + 1 << ". mjesec!\n";
+        cin >> temperatura;
+        t.izracunajTemperaturu(temperatura);
+        p.izracunajTemperaturu(temperatura);
+    }
+
+    t.Izvjestaj();
+    p.Izvjestaj();
     return 0;
 }
 
