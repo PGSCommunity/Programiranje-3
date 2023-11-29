b85fecbc6e0b6d3a2525426f57782103c572b8d4
Danin Sadžak <daninsadzak@gmail.com>
2023-11-29 12:29:23 +0100

    Vjezbe - Kompozicija - Postavljen 4. zadatak

---
+++ Vjezbe/Kompozicija/4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+Kreirati klasu Tekst, sa atributima i metodama koji će omogućiti ispis broja velikih slova, malih slova, 
+interpunkcija i razmaka. Ukupni broj znakova (velikih slova, malih slova, interpunkcija i razmaka) predstavlja 
+prosječan broj znakova jedne stranice knjige. Kreirati izvedenu klasu Knjiga sa atributima naziv i broj stranica.
+Potrebno je ispisati koliko bi znakova (u prosjeku ) sadržavala ta knjiga.
+*/
