218d646855848058a4e6b2775d9b63384b26e15a
Danin Sadžak <daninsadzak@gmail.com>
2024-02-03 15:07:58 +0100

    Vjezbe - Organizacija Koda - Postavljen 1. zadatak

---
+++ Vjezbe/Organizacija koda/1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    printf("Hello, world!\n");
+    return 0;
+}
+
+/*
+Sljedecu klasu postavi u pakete:
+class Broj{
+    int b;
+public:
+    void unos(){
+        cout<<"Unesite broj ";
+        cin>>b;
+    }  
+    int dvostruka(){
+        return 2*b;
+    }
+};  
+*/
