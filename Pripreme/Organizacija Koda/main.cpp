3294a67a4aa210147e545562c8fecad10a003cb7
Danin Sadžak <daninsadzak@gmail.com>
2024-02-04 12:27:01 +0100

    Pripreme - Organizacija Koda - Postavljen 1. zadatak

---
+++ Pripreme/Organizacija Koda/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "Hello, World!\n";
+	return 0;
+}
+
+/*
+1. Napisati program koji za ucitani cetverocifreni broj N racuna i ispisuje:
+	- tablicu množenja za taj broj
+	- faktorijel ucitanog broja 
+	- sumu parnih cifara ucitanog broja 
+*/
