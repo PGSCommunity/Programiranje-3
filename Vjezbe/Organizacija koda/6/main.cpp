b21b4d0796bfe85944ea20bf1509ff553734e2aa
Danin Sadžak <daninsadzak@gmail.com>
2024-02-04 12:04:32 +0100

    Vjezbe - Organizacija Koda - Postavljen 6. zadatak

---
+++ Vjezbe/Organizacija koda/6/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "Hello, World!\n";
+	return 0;
+}
+
+/*
+Sljedeci kod raspodijeli u pakete!
+#include<iostream>
+using namespace std;
+
+class Niz{
+	
+			int n, s = 0, p = 1, a[100];
+		
+		public:	
+		
+		Niz();	
+	void Unos();
+	int SumaProizvod();
+	
+	
+};
+
+	Niz :: Niz(){
+			cout  << "Unesi dimenziju niza\n";
+	cin >> n;
+				
+	}
+
+ void Niz :: Unos(){
+ 
+	cout  << "Unesi clanove niza\n";
+	for(int i = 0; i < n; i++){
+		
+		cin >> a[i];	
+ 	
+ 	 }
+ }
+
+ int Niz :: SumaProizvod(){
+ 
+	for(int i = 0; i < n; i++){
+		
+		if (a[i]%2==0)s += a [i];
+		if (a[i]%3!=0)p *= a [i];	
+ 	
+ 	 }
+ 	 	cout<<"Suma parnih brojeva niza je:\n"<<s<<"\nProizvod clanova niza koji nisu djeljivi sa 3 je:\n"<<p<<endl;
+ 	 }
+ 	 
+ 	 class Matrica{
+		
+			int m, SD = 0, Matrix[50][50];
+			public:
+		Matrica();	
+	void Unos();
+	int Dijagonala();
+	
+	
+};
+
+Matrica :: Matrica(){
+
+cout  << "Unesi dimenziju matrice\n";
+	cin >> m;
+	
+}
+	
+void Matrica :: Unos(){
+ 
+cout  << "Unesi clanove matrice\n";
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < m; j++){
+		cin >> Matrix[i][j];
+ 	
+ 	 }
+ }
+}
+
+ int Matrica :: Dijagonala(){
+ 
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < m; j++){
+		
+		if (i==j) SD += Matrix[i][j];
+
+	}
+}
+	cout<<"Suma elemenata matrice na glavnoj dijagonali je:\n"<<SD;
+}
+*/
