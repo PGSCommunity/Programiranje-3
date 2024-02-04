36fc788487901b62fcc683ec6ef0dad09c75cdfb
Danin Sadžak <daninsadzak@gmail.com>
2024-02-04 11:41:15 +0100

    Vjezbe - Organizacija Koda - Postavljen 5. zadatak

---
+++ Vjezbe/Organizacija koda/5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Hello, World!\n";
+	return 0;
+}
+
+/*
+Napisati program koji za ucitani broj ispisuje da li je on savrsen, 
+potom za dva ucitana broja pronalazi i ispisuje njihov NZD.
+Program treba da se sastoji od dvije klase sa po dvije metode ( unos i proracun) 
+*/
