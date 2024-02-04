ffd00fbdc246c774006bd3ff99227568416e8eb7
Danin Sadžak <daninsadzak@gmail.com>
2024-02-04 11:16:56 +0100

    Vjezbe - Organizacija Koda - Postavljen 4. zadatak

---
+++ Vjezbe/Organizacija koda/4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	cout << "Hello, world!\n";
+	return 0;
+}
+
+/*
+Napisati program koji ucitava trocifren broj.
+Program treba da, koristeci OOP (jedna klasa i 4 metode), ispise sumu cifara, proizvod reciprocnih vrijednosti cifara i da li je 
+broj Armstrongov
+*/
