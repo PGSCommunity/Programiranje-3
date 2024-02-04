33f7ecefe18885110740389f216c53aa34c3ce35
Danin Sadžak <daninsadzak@gmail.com>
2024-02-04 13:07:57 +0100

    Pripreme - Organizacija Koda - Postavljen 2. zadatak

---
+++ Pripreme/Organizacija Koda/2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "Hello, World!\n";
+	return 0;
+}
+
+/*
+2. Napisati program koji racuna povrsine jednakokrakog rougla i pravilne sestostrane piramide
+Parametri potrebni za proracune su konstante. U OOP te konstante su definirane kroz konstruktor.
+*/
